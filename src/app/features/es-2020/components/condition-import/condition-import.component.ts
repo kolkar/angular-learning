abe1de2372d4da7dacdf158717152de1b515b8c7
Yaroslav Diadko <yaroslav.diadko@softermii.com>
2020-04-24 12:18:19 +0300

    Finished es-2020 features

--- src/app/features/es-2020/components/condition-import/condition-import.component.ts
+++ src/app/features/es-2020/components/condition-import/condition-import.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// TODO: Organize code, discuss with Nikolay
+// TODO: Check https://www.freecodecamp.org/news/javascript-new-features-es2020/amp/
+/*
+Module Namespace Exports
+// Possible for imports
+import * as utils from './utils.mjs'
+// Added the same possibility for exports
+export * as utils from './utils.mjs'
+This possibility replaced with this code
+import * as utils from './utils.mjs'
+export { utils }*/
+
 @Component({
   selector: 'app-condition-import',
   templateUrl: './condition-import.component.html',
@@ -15,12 +27,14 @@ export class ConditionImportComponent implements OnInit {
     await this.dynamicImport();
 
     console.log(this.nullishCoalescing());
+
+    this.optionalChaining();
+    this.promiseAllSettled();
   }
 
-  // TODO: Check https://www.freecodecamp.org/news/javascript-new-features-es2020/amp/
-  /***
-  * Big int allows to store numbers more than pow(2, 53) - 1
-  * */
+  /**
+    * Big int allows to store numbers more than pow(2, 53) - 1
+    */
   private bigInt(): number {
     // Add "n" to end of number
     let bigInt = 9007199254740992;
@@ -35,11 +49,51 @@ export class ConditionImportComponent implements OnInit {
     console.log('dynamicFile', dynamicFile);
   }
 
+  /**
+   * Nullish coalescing return falsy in comparisons except null and undefined
+   */
   private nullishCoalescing() {
     return {
       falseValue: false ?? 'truth',
-      nullish: null ?? 'truth',
-      zero: 0 ?? 'truth',
+      truthValue: null ?? 'truth',
+      zeroValue: 0 ?? 'truth',
+      nanValue: NaN ?? 'truth',
+      truthValue2: false || 'truth',
+      truthValue3: null || 'truth',
+      truthValue4: 0 || 'truth',
+      truthValue5: NaN || 'truth',
     }
+  }
+
+  /**
+   * Using optional chaining we can specify optional properties
+   */
+  private optionalChaining() {
+    const x = {
+      prop1: {
+        prop2: 100
+      },
+    };
+
+    console.log(
+      x.prop1?.prop2,
+      x.prop1?.notExist,
+    );
+  }
+
+  /**
+   * Resolves when all promises resolves or rejected
+   */
+  private promiseAllSettled() {
+    const myPromiseArray = [
+      Promise.resolve(100),
+      Promise.reject(null),
+      Promise.reject(new Error('Error text')),
+    ];
+
+    Promise.allSettled(myPromiseArray)
+      .then(results => {
+        console.log(results);
+      });
   }
 }
